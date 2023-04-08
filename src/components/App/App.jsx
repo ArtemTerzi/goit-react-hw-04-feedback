b91578f992fde7f273fd8122a17ae7d24988ce2e
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-04-08 15:44:10 +0300

    ver 0.01

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,43 @@
 import Feedback from '../Feedback/Feedback';
-import React, { Component } from 'react';
+import { useState } from 'react';
 
-// export const App = () => {
-//   return;
-// };
+export const App = () => {
+  const [good, setGoodFeedback] = useState(0);
+  const [neutral, setNeutralFeedback] = useState(0);
+  const [bad, setBadFeedback] = useState(0);
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const stateShape = { good, neutral, bad };
+
+  const onLeaveFeedback = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'good':
+        setGoodFeedback(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutralFeedback(prev => prev + 1);
+        break;
+      case 'bad':
+        setBadFeedback(prev => prev + 1);
+        break;
+      default:
+        return console.error(`Name - ${name} is not defind`);
+    }
   };
 
-  onLeaveFeedback = e => {
-    this.setState(prevState => ({
-      [e.target.name]: prevState[e.target.name] + 1,
-    }));
-  };
+  const countTotalFeedback = () =>
+    Object.values(stateShape).reduce((acc, el) => acc + el, 0);
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((acc, el) => acc + el, 0);
-
-  countPositiveFeedbackPercentage = () => {
-    const goodPercentage = Math.round(
-      (this.state.good / this.countTotalFeedback()) * 100
-    );
+  const countPositiveFeedbackPercentage = () => {
+    const goodPercentage = Math.round((good / countTotalFeedback()) * 100);
     return !goodPercentage ? 0 : goodPercentage;
   };
 
-  render() {
-    return (
-      <Feedback
-        state={this.state}
-        onLeaveFeedback={this.onLeaveFeedback}
-        countTotalFeedback={this.countTotalFeedback}
-        countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
-      />
-    );
-  }
-}
+  return (
+    <Feedback
+      state={stateShape}
+      onLeaveFeedback={onLeaveFeedback}
+      countTotalFeedback={countTotalFeedback}
+      countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+    />
+  );
+};
